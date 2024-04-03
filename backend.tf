e6f7163d6b17e0a0ee895eeed6699aae3b37327e
spanga1 <164720420+spanga1@users.noreply.github.com>
2024-04-03 15:59:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,15 @@ terraform {
         resource_group_name  = "rg-azuba-ICS"
         storage_account_name = "nageshtest1"
         container_name       = "terraform"
-        key                 = "terraform.tfstate"
+        key                  = "terraform-${pipeline_id}.tfstate"  # Using pipeline ID as part of the state file name
     }    
 }
+
+#terraform {
+#    backend "azurerm" {
+#        resource_group_name  = "rg-azuba-ICS"
+#        storage_account_name = "nageshtest1"
+#        container_name       = "terraform"
+#        key                 = "terraform.tfstate"
+#    }    
+#}
