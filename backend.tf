27154439b2edefb065693c1c75bcd35783e5fb1d
spanga1 <164720420+spanga1@users.noreply.github.com>
2024-04-12 13:45:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
-    backend "azurerm" {
-        resource_group_name  = "rg-azuba-ICS"
-        storage_account_name = "nageshtest1"
-        container_name       = "terraform"
-        key                 = "terraform.tfstate"
-    }    
+  backend "azurerm" {
+    resource_group_name   = "rg-test-SQL"
+    storage_account_name  = "nageshtest3"
+    container_name        = "terraform"
+    key                   = "terraform.tfstate"
+
+    use_msi = true // Use Managed Identity for authentication
+  }
 }
